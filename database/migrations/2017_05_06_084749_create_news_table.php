8676266f5dc1506d846155ac983465c50158ebc7
Sasha <hagrida@yandex.ru>
2017-05-06 11:59:38 +0300

    Залил миграции в БД

--- database/migrations/2017_05_06_084749_create_news_table.php
+++ database/migrations/2017_05_06_084749_create_news_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateNewsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,13 +14,17 @@ class CreateUsersTable extends Migration
     public function up()
     {
         try {
-            Schema::create('users', function (Blueprint $table) {
+            Schema::create('news', function (Blueprint $table) {
                 $table->increments('id');
-                $table->string('name');
-                $table->string('email')->unique();
-                $table->string('password');
-                $table->rememberToken();
+                $table->integer('user_id')->unsigned();
+                $table->string('title');
+                $table->string('slug')->unique();;
+                $table->text('content');
                 $table->timestamps();
+
+                $table->foreign('user_id')->references('id')->on('users')
+                    ->onDelete('cascade')
+                    ->onUpdate('cascade');
             });
         }catch(\Exception $e){
             $this->down();
@@ -35,6 +39,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('news');
     }
 }
